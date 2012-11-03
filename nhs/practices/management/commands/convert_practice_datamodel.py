424b8aa8001849ca5d56219be879f9fa9927dd0c
David Miller <david@deadpansincerity.com>
2012-11-03 13:27:37 +0000

    Add conversion command

---
+++ nhs/practices/management/commands/convert_practice_datamodel.py
@@ -0,0 +1,21 @@
+"""
+Command to link the existing practice data to Mapit Postcodes
+"""
+from django.core.management.base import BaseCommand
+
+from mapit.models import Postcode
+
+from practices.models import Practice
+
+
+
+class Command(BaseCommand):
+    def handle(self,**options):
+        for practice in practices.models.all():
+            pc = Postcode.objects.filter(postcode=practice.postcode)
+            if len(pc) == 0:
+                import pdb; pdb.set_trace()
+            else:
+                pc = pc[0]
+            practice.pc = pc
+            practice.save()
