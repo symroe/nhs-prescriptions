eb97ef18bf9dc39088f9e63a8b4db317d515c20c
Ross Jones <rossdjones@gmail.com>
2012-12-17 14:07:38 +0000

    Update practices to have a FK to their CCG, and a management command for binding them

---
+++ nhs/ccgs/management/commands/bind_practices.py
@@ -0,0 +1,22 @@
+import sys
+import csv
+import optparse
+from xml.dom.minidom import parseString
+from django.contrib.gis.geos import MultiPolygon, Polygon
+
+from django.core.management.base import BaseCommand
+
+from nhs.ccgs.models import CCG
+from nhs.practices.models import Practice
+
+class Command(BaseCommand):
+
+    def handle(self, **options):
+        for practice in Practice.objects.all():
+            if not practice.location:
+                continue
+
+            ccgs = CCG.objects.filter(poly__contains=practice.location)
+            if ccgs:
+                practice.ccg = ccgs[0]
+                practice.save()
--- nhs/practices/models.py
+++ nhs/practices/models.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from django.contrib.gis.db import models as geo_models
+from nhs.ccgs.models import CCG
 
 class Practice(geo_models.Model):
     practice = models.CharField(blank=True, max_length=100, primary_key=True)
     name = models.CharField(blank=True, max_length=255)
-    location = geo_models.PointField(spatial_index=True, geography=True, null=True)
+    location = geo_models.PointField(spatial_index=True, geography=False, null=True)
     postcode = models.CharField(blank=True, max_length=10)
     address = models.TextField(blank=True)
+    ccg = models.ForeignKey(CCG, null=True)
+    objects = geo_models.GeoManager()
 
     def __unicode__(self):
         return "%s (%s)" % (self.pk, self.name)
