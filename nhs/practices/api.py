f4a8b7b63340363dd15a4abaefa87d905a2afcbe
Sym Roe <sym.roe@talusdesign.co.uk>
2013-01-27 14:32:18 +0000

    app pc to API

--- nhs/practices/api.py
+++ nhs/practices/api.py
@@ -1,9 +1,18 @@
+from tastypie import fields
 from tastypie.contrib.gis.resources import ModelResource as GeoModelResource
 
 from models import Practice
+from mapit.models import Postcode
 
+class MapitPostCodeResource(GeoModelResource):
+    class Meta:
+        queryset = Postcode.objects.all()
 
 class PracticeResource(GeoModelResource):
+    pc = fields.ToOneField(MapitPostCodeResource, 'pc', null=True)
+    
     class Meta:
             model = Practice
             queryset = Practice.objects.all()
+
+
