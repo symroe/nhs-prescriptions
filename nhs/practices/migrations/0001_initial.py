c97e894f82c5948974632a2202f593695e554828
David Miller <david@deadpansincerity.com>
2013-01-29 03:33:50 +0000

    Fix Migrations for practices. Truncating the table and deleting from south_migrationhistory before running the new initial migration advised.

--- nhs/practices/migrations/0001_initial.py
+++ nhs/practices/migrations/0001_initial.py
@@ -12,9 +12,11 @@ class Migration(SchemaMigration):
         db.create_table('practices_practice', (
             ('practice', self.gf('django.db.models.fields.CharField')(max_length=100, primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
-            ('location', self.gf('django.contrib.gis.db.models.fields.PointField')(null=True, geography=True)),
             ('postcode', self.gf('django.db.models.fields.CharField')(max_length=10, blank=True)),
+            ('pc', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['mapit.Postcode'], null=True, blank=True)),
+            ('imd', self.gf('django.db.models.fields.FloatField')(null=True, blank=True)),
             ('address', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('ccg', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['ccgs.CCG'], null=True)),
         ))
         db.send_create_signal('practices', ['Practice'])
 
@@ -25,11 +27,60 @@ class Migration(SchemaMigration):
 
 
     models = {
+        'ccgs.ccg': {
+            'Meta': {'object_name': 'CCG'},
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '5'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lsoa_count': ('django.db.models.fields.IntegerField', [], {}),
+            'name': ('django_extensions.db.fields.AutoSlugField', [], {'allow_duplicates': 'False', 'max_length': '50', 'separator': "u'-'", 'blank': 'True', 'populate_from': "'title'", 'overwrite': 'False'}),
+            'poly': ('django.contrib.gis.db.models.fields.MultiPolygonField', [], {}),
+            'population': ('django.db.models.fields.IntegerField', [], {}),
+            'region': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'mapit.area': {
+            'Meta': {'ordering': "('name', 'type')", 'object_name': 'Area'},
+            'country': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'areas'", 'null': 'True', 'to': "orm['mapit.Country']"}),
+            'generation_high': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'final_areas'", 'null': 'True', 'to': "orm['mapit.Generation']"}),
+            'generation_low': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'new_areas'", 'null': 'True', 'to': "orm['mapit.Generation']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'parent_area': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'children'", 'null': 'True', 'to': "orm['mapit.Area']"}),
+            'type': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'areas'", 'to': "orm['mapit.Type']"})
+        },
+        'mapit.country': {
+            'Meta': {'object_name': 'Country'},
+            'code': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '1'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'})
+        },
+        'mapit.generation': {
+            'Meta': {'object_name': 'Generation'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'mapit.postcode': {
+            'Meta': {'ordering': "('postcode',)", 'object_name': 'Postcode'},
+            'areas': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'postcodes'", 'blank': 'True', 'to': "orm['mapit.Area']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.contrib.gis.db.models.fields.PointField', [], {'null': 'True'}),
+            'postcode': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '7', 'db_index': 'True'})
+        },
+        'mapit.type': {
+            'Meta': {'object_name': 'Type'},
+            'code': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '3'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
         'practices.practice': {
             'Meta': {'object_name': 'Practice'},
             'address': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'location': ('django.contrib.gis.db.models.fields.PointField', [], {'null': 'True', 'geography': 'True'}),
+            'ccg': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['ccgs.CCG']", 'null': 'True'}),
+            'imd': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'pc': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mapit.Postcode']", 'null': 'True', 'blank': 'True'}),
             'postcode': ('django.db.models.fields.CharField', [], {'max_length': '10', 'blank': 'True'}),
             'practice': ('django.db.models.fields.CharField', [], {'max_length': '100', 'primary_key': 'True'})
         }
